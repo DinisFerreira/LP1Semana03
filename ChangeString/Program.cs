66f3a3d82a47f2e958557dbdd4fff9e503af87df
Dinis <dinisvferreira@gmail.com>
2024-03-07 16:32:32 +0000

    add questions

---
+++ ChangeString/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ChangeString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please write a string:");
+            string str = Console.ReadLine();
+
+            Console.WriteLine("give me one character");
+            string Scar = Console.ReadLine();
+            
+        }
+    }
+}
